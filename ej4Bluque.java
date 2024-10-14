d66fea2bdecda4e3e70740a38eb9d8c5105afbb7
LuccaWicher <luccaw21@gmail.com>
2024-10-14 19:27:32 -0300

    Add files via upload

---
+++ ej4Bluque.java
@@ -0,0 +1,16 @@
+import java.util.Scanner;
+public class ej4Bluque {
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+        double totalVentas = 0;
+
+        for (int dia = 1; dia <= 7; dia++) {
+            System.out.print("Ingresa las ventas del día " + dia + ": ");
+            double ventas = scanner.nextDouble();
+            totalVentas += ventas;
+        }
+
+        System.out.println("El total de ventas de la semana es: $" + totalVentas);
+        scanner.close();
+    }
+}
